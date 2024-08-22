58408e6b859c8100b6941adba2dcbdc7d82dd6e0
physnerds <amitbashyal@gmail.com>
2024-08-22 15:09:26 -0500

    Write decompressed data into ROOT histogram

--- tests/decompress_sz3_test.cxx
+++ tests/decompress_sz3_test.cxx
@@ -7,28 +7,46 @@
 #include "compression_interface/Utilities.hxx"
 #include "compression_interface/WriteConfigParameters.h"
 #include "compression_interface/SZ3CompressionTools.hxx"
+#include "TFile.h"
+#include "TH2D.h"
+#include "TH1D.h"
 
 const std::string rfilename = "test_SZ3_compression.root";
 const std::string ntuplename = "Compressions";
+const int n_channels = 10240;
+const int n_ticks = 5859;
 int main(){
     auto reader = ROOT::Experimental::RNTupleReader::Open(ntuplename.c_str(),rfilename.c_str());
     reader->PrintInfo();
     auto nentries = reader->GetNEntries();
-    auto fieldOrigData = reader->GetView<std::vector<double>>("out_data");
+    TH2D* decompressed_hist = new TH2D("decompressed_value","decompressed_value",n_channels,0,n_channels-1,n_ticks,0,n_ticks-1);
+    TH1D* comp_ratio = new TH1D("compression_ratio","compression_ratio",n_channels,0,n_channels-1);
+
+    std::cout<<"Total Entries "<<nentries<<std::endl;
     auto fieldCompressedData = reader->GetView<std::vector<char>>("compressed_data");
     auto param = reader->GetView<std::string>("parameters");
 
-    for(size_t entry = 0; entry<2;entry++){
+    for(size_t entry = 0; entry<n_channels;entry++){
         std::string temp_param = param(entry);
         std::string temp_jfilename = "temp_jfilename.json";
         nlohmann::json jobj = readJSONString(temp_param);
+        int channel_number =  static_cast<int>(jobj.at("ORIGINAL DSET"));
+        double ratio = static_cast<double>(jobj.at("COMPRESSION_RATIO"));
         writeJSON(jobj,temp_jfilename);
-        std::cout<<jobj.dump()<<std::endl;
+        //std::cout<<temp_param<<std::endl;
         std::vector<char> compressed_data = fieldCompressedData(entry);
         SZ3Compressor sz3compress(temp_jfilename);
         //now do the decompression
         double* decompressed_data = sz3compress.decompress<double>(compressed_data.data());
-
-
+        for(int j=0;j<n_ticks;j++){ //y axis is the number of ticks
+            decompressed_hist->SetBinContent(channel_number,j,decompressed_data[j]);
+        }
+        comp_ratio->SetBinContent(channel_number,ratio);
+        
     }
+    TFile* rfile = new TFile("decompression_info.root","RECREATE");
+    rfile->cd();
+    decompressed_hist->Write();
+    comp_ratio->Write();
+    rfile->Close();
 }
