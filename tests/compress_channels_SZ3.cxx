cbb3a76d53d17ee479c574dd1f403dac00eb4b9f
physnerds <amitbashyal@gmail.com>
2024-08-23 07:32:19 -0500

    clean ups

--- tests/compress_channels_SZ3.cxx
+++ tests/compress_channels_SZ3.cxx
@@ -11,12 +11,10 @@ namespace SZ3{
 }
 
 const std::string filename ="/home/abashyal/git_compression/compressiontests/new_magnify2-28052-20833h_orig_1D.h5" ; //"/home/abashyal/compressiontests/waveforms_ver2.h5";
-const std::string outfilename = "test_SZ3_compression.root" ; 
+const std::string outfilename = "test_SZ3_compression_err20.root" ; 
 const int tot_channels = 10240;
 int main(){ 
     H5::H5File file(filename,H5F_ACC_RDONLY);
-    //std::string dsetname = "waveform_1";
-    //std::vector<double> out_data = ReturnH5Data<double>(file,dsetname);
     //we want to write the compressed data and original data in the RNTuple File...
     //so we create the model schema....
     //ROOT version might cause API issues...check on your own risk..
@@ -34,10 +32,10 @@ int main(){
         *fieldOutData = out_data;
         //we are creating a json file first...
         //TODO : Maybe use err_bound also as a parameter to do the test?
-        double abs_err_bound = 4 ;
-        double rel_err_bound = 4;
-        double psnr_err_bound = 4;
-        double l2norm_err_bound = 4;
+        double abs_err_bound = 8 ;
+        double rel_err_bound = 8;
+        double psnr_err_bound = 8;
+        double l2norm_err_bound = 8;
         std::string str_al_val = std::string(SZ3::enum2Str<SZ3::ALGO>(SZ3::ALGO_INTERP_LORENZO));
         std::string str_al_eb = std::string(SZ3::enum2Str<SZ3::EB>(SZ3::EB_ABS));
         std::string str_al_ial = std::string(SZ3::enum2Str<SZ3::INTERP_ALGO>(SZ3::INTERP_ALGO_CUBIC));
@@ -45,8 +43,6 @@ int main(){
         std::string str_rel_err_bound = std::to_string(abs_err_bound);
         std::string str_psnr_err_bound = std::to_string(abs_err_bound);
         std::string str_l2norm_err_bound = std::to_string(l2norm_err_bound);
-        //std::string temp_name = "params_sz3_"+str_al_val+"_"+
-        //    str_al_eb+"_"+str_al_ial+"_"+str_abs_err_bound+"_"+str_rel_err_bound+"_"+str_psnr_err_bound+"_"+str_l2norm_err_bound;
         std::string temp_name = "params_sz3";
         std::string jfilename = temp_name+".json";
         WriteJSONConfig("SZ3",
@@ -65,8 +61,6 @@ int main(){
         char* compressed_data = sz3compress.compress<double>(out_data);
         double* decompressed_data = sz3compress.decompress<double>(compressed_data);
         
-        //auto ksval = CalculateKSVal<int>(out_data.size(),out_data.data(),decompressed_data);
-
         std::size_t compressed_size = sz3compress.GetCompressedSize();
         double comp_ratio = double(sizeof(int)*out_data.size()/(sizeof(char)*compressed_size));
         jfile["COMPRESSION_RATIO"] = comp_ratio;
@@ -74,12 +68,7 @@ int main(){
 
         //this is to make sure that the json content is updated before writing into rntuple.
         sz3compress.UpdateJSONContent(jfile);
-        //double tranformed_error = jfile.at("Transformed_Error");
-        //std::string rfilename = temp_name+std::to_string(tranformed_error);
-
-        //DrawGraphs(out_data.data(),decompressed_data,out_data.size(),temp_name+".png",ksval,comp_ratio);
-        //dump the compression parameter....
-        //sz3compress.dumpJSONContent();                    
+                           
         param->clear();
         *param = jfile.dump();
 
